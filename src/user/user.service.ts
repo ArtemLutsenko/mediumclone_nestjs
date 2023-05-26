42dcc31ec8658a8f16e62892c825c675c49b28a5
ArtemLutsenko <artem.lutsenko@gmail.com>
2023-05-26 16:38:52 +0300

    auth module finish

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { Body, HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { CreateUserDto } from "@app/user/dto/createUser.dto";
 import { UserEntity } from "@app/user/user.entity";
 import { InjectRepository } from "@nestjs/typeorm";
@@ -6,6 +6,8 @@ import { Repository } from "typeorm";
 import {sign} from "jsonwebtoken";
 import { JWT_SECRET } from "@app/config";
 import { UserResponseInterface } from "@app/user/types/userResponse.interface";
+import {compare} from 'bcrypt'
+import { UpdateUserDto } from "@app/user/dto/updateUser.dto";
 
 @Injectable()
 export class UserService{
@@ -14,9 +16,50 @@ export class UserService{
   }
 
   async createUser(createUserDto: CreateUserDto): Promise<UserEntity>{
+    const userByEmail = await this.userRepository.findOne({
+      where: {email:createUserDto.email}
+    })
+    const userByUsername = await this.userRepository.findOne({
+      where: {username:createUserDto.username}
+    })
+    if(userByEmail || userByUsername){
+      throw new HttpException('Email or username are taken', HttpStatus.UNPROCESSABLE_ENTITY)
+    }
     const newUser = new UserEntity()
     Object.assign(newUser, createUserDto)
     return await this.userRepository.save(newUser)
+  }
+
+  async loginUser(loginUserDto): Promise<UserEntity>{
+    const user = await this.userRepository.findOne({
+      where: { email: loginUserDto.email },
+      select: ['id', "email", "username", "image", "password", "bio"]
+    })
+
+    if(!user){
+      throw new HttpException('Credential are not valid', HttpStatus.UNPROCESSABLE_ENTITY)
+    }
+
+    const  isPasswordCorrect = await compare(loginUserDto.password, user.password)
+
+    if(!isPasswordCorrect){
+      throw new HttpException('Credential are not valid', HttpStatus.UNPROCESSABLE_ENTITY)
+    }
+
+    delete user.password
+    return user
+  }
+
+  async updateUser(userId: number, updateUserDto: UpdateUserDto): Promise<UserEntity>{
+    const user = await  this.findById(userId)
+    Object.assign(user, updateUserDto)
+    return await this.userRepository.save(user)
+  }
+
+  async findById(id: number){
+    return this.userRepository.findOne({
+      where: {id}
+    })
   }
 
   generateJwt(user: UserEntity): string{
